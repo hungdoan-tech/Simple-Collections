34598c5d6788665f310e799ea8d193b3f7f29f00
hungdoan-tech <hungdoan426@gmail.com>
2020-10-06 18:46:28 +0700

    Build my own generic linked list API in Java - A idle afternoon

---
+++ My-Own-Linked-List-API/src/com/learnJava/myOwnLinkedList/genericLinkedList/GenericNode.java
@@ -0,0 +1,38 @@
+package com.learnJava.myOwnLinkedList.genericLinkedList;
+
+// Create a wrapper for T type with Generic Class
+public class GenericNode<T> {
+	private T data;
+	private GenericNode<T> nextNode;
+	private GenericNode<T> previousNode;
+	
+	public T getData() {
+		return this.data;
+	}
+	
+	public void setData(T data) {
+		this.data = data;
+	}
+	
+	public GenericNode<T> getNextNode() {
+		return nextNode;
+	}
+	
+	public void setNextNode(GenericNode<T> nextNode) {
+		this.nextNode = nextNode;
+	}
+	
+	public GenericNode<T> getPreviousNode() {
+		return previousNode;
+	}
+	
+	public void setPreviousNode(GenericNode<T> previousNode) {
+		this.previousNode = previousNode;
+	}
+	
+	public GenericNode(T data) {
+		this.data = data;
+		this.nextNode = null;
+		this.previousNode = null;
+	}
+}
