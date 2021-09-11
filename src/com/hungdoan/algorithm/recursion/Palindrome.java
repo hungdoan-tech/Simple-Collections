b652e54f411af23aecd96e66f31847470dc574cd
hungdoan-tech <hungdoan426@gmail.com>
2021-09-11 23:49:52 +0700

    Solve some simple recursion problems

---
+++ src/com/hungdoan/algorithm/recursion/Palindrome.java
@@ -0,0 +1,23 @@
+package com.hungdoan.algorithm.recursion;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+        String originalText = "akaka";
+        boolean isPalindrome = palindrome.isPalindrome(originalText);
+        System.out.println(isPalindrome);
+    }
+
+    public boolean isPalindrome(String text) {
+        int length = text.length();
+        System.out.println(text);
+        if (length == 0 || length == 1) {
+            return true;
+        }
+        if (text.charAt(0) == text.charAt(length - 1)) {
+            return isPalindrome(text.substring(1, length - 1));
+        }
+        return false;
+    }
+}
